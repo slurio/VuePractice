726feb066a5f658528abc038b9d117b13ff6d675
slurio <samantha.lurio@gmail.com>
2021-05-21 19:00:52 -0400

    created submit feature to search

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="search">
     <h1>This is the search page</h1>
-    <form>
+    <form @submit.prevent="searchInput">
       <input
         class="searchBar"
         type="text"
         placeholder="Type in here :)"
-        v-model="searchTerm"
+        v-model="searchObj.searchTerm"
       />
       <br />
-      <select v-model="departmentSelected">
+      <select v-model="searchObj.departmentSelected">
         <option disabled>Please select one</option>
         <option v-for="(department, index) in departments" :key="index">
           {{ department["displayName"] }}
@@ -17,22 +17,23 @@
       </select>
       <span>Department</span>
       <label>Medium</label>
-      <input type="text" v-model="medium" />
+      <input type="text" v-model="searchObj.medium" />
       <br />
       <label>Artist</label>
-      <input type="text" v-model="artist" />
+      <input type="text" v-model="searchObj.artist" />
       <br />
       <label>Location</label>
-      <input type="text" v-model="location" />
+      <input type="text" v-model="searchObj.location" />
       <br />
       <div class="checkBoxFields">
-        <input type="checkbox" v-model="permanentCollection" />
+        <input type="checkbox" v-model="searchObj.permanentCollection" />
         <label>Part of the permanent collection</label>
-        <input type="checkbox" v-model="currentlyOnView" />
+        <input type="checkbox" v-model="searchObj.currentlyOnView" />
         <label>Currently on view</label>
-        <input type="checkbox" v-model="hasImages" />
+        <input type="checkbox" v-model="searchObj.hasImages" />
         <label>Must have images</label>
       </div>
+      <input type="submit" value="Submit"/>
     </form>
   </div>
 </template>
@@ -47,16 +48,34 @@ import axios from "axios";
 export default {
   data() {
     return {
-      searchTerm: "",
-      departmentSelected: "",
-      permanentCollection: false,
-      currentlyOnView: false,
-      hasImages: false,
-      medium: "",
-      location: "",
-      artist: "",
+      // searchTerm: "",
+      // departmentSelected: "",
+      // permanentCollection: false,
+      // currentlyOnView: false,
+      // hasImages: false,
+      // medium: "",
+      // location: "",
+      // artist: "",
       departments: [],
+      searchObj: this.createFreshObj(),
     };
+  },
+  methods: {
+    createFreshObj() {
+      return {
+        searchTerm: "",
+        departmentSelected: "",
+        permanentCollection: false,
+        currentlyOnView: false,
+        hasImages: false,
+        medium: "",
+        location: "",
+        artist: "",
+      }
+    },
+    searchInput() {
+      console.log(this.searchObj);
+    }
   },
   created() {
     axios
@@ -70,7 +89,6 @@ export default {
         console.log(error.response);
       });
   },
-  
 };
 </script>
 
