1ee3fc201bf849e3110cbf2bd658bf9ef369df68
slurio <samantha.lurio@gmail.com>
2021-05-25 11:55:42 -0400

    downloard vue router and updated routes file

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,9 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import Vue from "vue";
+import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Search from "../views/Search.vue";
+
+Vue.use(VueRouter);
 
 const routes = [
   {
@@ -10,16 +14,13 @@ const routes = [
   {
     path: "/search",
     name: "Search",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Search.vue"),
+    component: Search,
   },
 ];
 
-const router = createRouter({
-  history: createWebHashHistory(),
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
   routes,
 });
 
